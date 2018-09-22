5463a79247b19019578c615bb1281b2c68d35515
Megan Sullivan <msulliv@thoughtworks.com>
2018-09-21 17:50:09 -0700

    [Megan] Fix: Display win message when player wins

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ class Game extends React.Component {
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
-        const winner = calculateWinner(current);
+        const winner = calculateWinner(current.squares);
 
         let status;
         if (winner) {
